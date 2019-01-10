cd324e051e0b0e6546a09290c8fef5dd48595a15
tjjfvi <tjjfvi@tjjfvi.com>
2019-01-10 08:43:57 -0500

    non category props can have children

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const url = "https://github.com/Ultimaker/Cura/raw/master/resources/definitions/
 router.get("/", async (req, res) => {
 	let { settings } = JSON.parse(await request(url));
 
-	let f = obj => [].concat(...Object.entries(obj).map(([k, v]) => v.type === "category" ? f(v.children) : [k]))
+	let f = obj => [].concat(...Object.entries(obj).map(([k, v]) => [
+		...f(v.children || []),
+		...(v.type === "category" ? [] : [k])
+	]))
 
 	res.set("Content-Type", "text/plain").send(f(settings).filter(k => k !== "machine_start_gcode" && k !== "machine_end_gcode").map(k => `; ${k} = {${k}}`).join("\n"));
 })
